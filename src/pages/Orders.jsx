606d23c9bfa64333692df8a6a20e22807da4254f
Doan Hoang Huy <92454872+HwiHwy@users.noreply.github.com>
2023-11-21 16:28:38 +0700

    a

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,17 +1,118 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { GridComponent, ColumnsDirective, ColumnDirective, Resize, Sort, ContextMenu, Filter, Page, ExcelExport, PdfExport, Edit, Inject } from '@syncfusion/ej2-react-grids';
 
 import { ordersData, contextMenuItems, ordersGrid } from '../data/dummy';
 import { Header } from '../components';
+import product6 from "./../data/product6.jpg";
+import { useDispatch, useSelector } from 'react-redux';
 
 const Orders = () => {
-  const editing = { allowDeleting: true, allowEditing: true };
+  const editing = { allowDeleting: true, allowEditing: false };
+
+
+  const ordersGrids = [
+    {
+      field: 'customerId',
+      headerText: 'customerId',
+      width: '150',
+      editType: 'dropdownedit',
+      textAlign: 'Center',
+    },
+    {
+      field: 'CustomerName',
+      headerText: 'Customer Name',
+      width: '150',
+      textAlign: 'Center',
+    },
+    {
+      field: 'totalPrice',
+      headerText: 'totalPrice',
+      format: 'C2',
+      textAlign: 'Center',
+      editType: 'numericedit',
+      width: '150',
+    },
+    {
+      field: 'id',
+      headerText: 'Order ID',
+      width: '120',
+      textAlign: 'Center',
+    },
+    {
+      field: 'status',
+      headerText: 'status',
+      width: '120',
+      textAlign: 'Center',
+    },
+
+    {
+      field: 'address',
+      headerText: 'Location',
+      width: '150',
+      textAlign: 'Center',
+    },
+    {
+      field: 'orderTime',
+      headerText: 'orderTime',
+      width: '150',
+      textAlign: 'Center',
+    },
+  ];
+
+
+
+  const [ordersData, setOrdersData] = useState([]);
+  const [error, setError] = useState(null);
+
+  const [loading, setLoading] = useState(true);
+
+  const [combinedData, setCombinedData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const ordersResponse = await fetch('https://silent257-001-site1.etempurl.com/api/Orders');
+        const customersResponse = await fetch('https://silent257-001-site1.etempurl.com/api/Customers/GetAll');
+        
+        const ordersData = await ordersResponse.json();
+        const customerData = await customersResponse.json();
+
+        const combinedData = ordersData.map(order => ({
+          ...order,
+          CustomerName: getCustomerName(customerData, order.customerId),
+        }));
+
+        setCombinedData(combinedData);
+        setLoading(false);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        setError('Error fetching data. Please try again.');
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const getCustomerName = (customerData, customerId) => {
+    const customer = customerData.find(customer => customer.id === customerId);
+    return customer ? `${customer.firstName} ${customer.lastName}` : '';
+  };
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+  console.log(combinedData);
   return (
     <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
       <Header category="Page" title="Orders" />
       <GridComponent
         id="gridcomp"
-        dataSource={ordersData}
+        dataSource={combinedData}
         allowPaging
         allowSorting
         allowExcelExport
@@ -20,8 +121,7 @@ const Orders = () => {
         editSettings={editing}
       >
         <ColumnsDirective>
-          {/* eslint-disable-next-line react/jsx-props-no-spreading */}
-          {ordersGrid.map((item, index) => <ColumnDirective key={index} {...item} />)}
+          {ordersGrids.map((item, index) => <ColumnDirective key={index} {...item} />)}
         </ColumnsDirective>
         <Inject services={[Resize, Sort, ContextMenu, Filter, Page, ExcelExport, Edit, PdfExport]} />
       </GridComponent>
